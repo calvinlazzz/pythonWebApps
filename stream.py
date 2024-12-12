aaaed0c0843f1c8a793bd3aabaf6f722187bd6d7
Calvin La <calvin.la@clover.com>
2024-12-12 13:15:38 -0800

    added data

--- stream.py
+++ stream.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas 
+
+data = {
+    'Series_1': [1, 2, 3, 4, 5],
+    'Series_2': [10, 20, 30, 40, 50]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Streamlit App')
 st.subheader('This is a subheader')
 st.write('This is a normal text')
+st.write(df)
